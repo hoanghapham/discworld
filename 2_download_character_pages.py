3bf575be9f9a89784117367933ef31d98ded437c
hoanghapham <hoangha.20021992@gmail.com>
2020-07-07 16:14:57 +0700

    temporarily remove progress checking

--- 2_download_character_pages.py
+++ 2_download_character_pages.py
@@ -20,29 +20,31 @@ args = parser.parse_args()
 char_list = pd.read_csv('data/raw/character_list.csv')
 
 save_interval = 5
-start_point = 0
-end_point = -1
 
 if args.start_point is not None:
     start_point = args.start_point
+else:
+    start_point = 0
 
 if args.end_point is not None:
     end_point = args.end_point
+else:
+    end_point = -1
 
 if os.path.exists('progress') == False:
     os.makedirs('progress')
 
-try:
-    with open('progress/characters_progress.json', 'r') as obj:
-        last_progress = json.load(obj)
-        print('Last saved character index: {}'.format(last_progress['char_index']))
-        start_point = last_progress['char_index']
+# try:
+#     with open('progress/characters_progress.json', 'r') as obj:
+#         last_progress = json.load(obj)
+#         print('Last saved character index: {}'.format(last_progress['char_index']))
+#         start_point = last_progress['char_index']
         
-except FileNotFoundError as error:
-    print('Progress file not found. Start from beginning.')
+# except FileNotFoundError as error:
+#     print('Progress file not found. Start from beginning.')
     
-except json.JSONDecodeError as error:
-    print('No progress found. Start from beginning')
+# except json.JSONDecodeError as error:
+#     print('No progress found. Start from beginning')
 
 crawl_list = char_list.url[start_point:end_point]
 
@@ -59,7 +61,7 @@ for counter, url in enumerate(crawl_list, start=1):
     
     utils.save_html(page_res, 'character_pages/', char + '.html')
     
-    char_progress = {'char_index': counter - 1}
+    char_progress = {'char_index': start_point + counter - 1}
     
     if counter % save_interval == 0:
         with open('progress/characters_progress.json', 'w') as obj:
